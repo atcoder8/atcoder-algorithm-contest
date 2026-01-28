4e4be1cb49556f098c06316fa64591c82b4988d7
pencoder <atcoder8@gmail.com>
2026-01-28 23:00:25 +0900

    Solved ABC442-F.

--- judge-202510/abc442/src/bin/f.rs
+++ judge-202510/abc442/src/bin/f.rs
@@ -1,7 +1,32 @@
-use proconio::input;
+use itertools::enumerate;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        
+        n: usize,
+        ss: [Chars; n],
     }
+
+    let mut dp = vec![0_usize; n + 1];
+    let mut next_dp = vec![0_usize; n + 1];
+    for s in &ss {
+        let mut acc_min_cost = dp[n];
+        let mut add_cost = s.iter().filter(|&&ch| ch == '#').count();
+        next_dp[n] = acc_min_cost + add_cost;
+        for (i, &ch) in enumerate(s).rev() {
+            acc_min_cost = acc_min_cost.min(dp[i]);
+            if ch == '#' {
+                add_cost -= 1;
+            } else {
+                add_cost += 1;
+            }
+
+            next_dp[i] = acc_min_cost + add_cost;
+        }
+
+        std::mem::swap(&mut dp, &mut next_dp);
+    }
+
+    let min_cost = *dp.iter().min().unwrap();
+    println!("{min_cost}");
 }
