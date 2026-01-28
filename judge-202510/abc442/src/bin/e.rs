de39c5e7a3f4046d592d7eb0880d14b4467be1c8
pencoder <atcoder8@gmail.com>
2026-01-28 22:36:24 +0900

    Solved ABC442.

--- judge-202510/abc442/src/bin/e.rs
+++ judge-202510/abc442/src/bin/e.rs
@@ -1,8 +1,5 @@
 use itertools::Itertools;
 use proconio::{input, marker::Usize1};
-use superslice::Ext;
-
-use crate::amplitude::Amplitude;
 
 fn main() {
     input! {
@@ -11,15 +8,14 @@ fn main() {
         ab: [(Usize1, Usize1); q],
     }
 
-    // 時計回りに合わせるために左右反転
-    let amplitudes = xy.iter().map(|&(x, y)| Amplitude::new(-x, y)).collect_vec();
-
-    let mut sorted_amplitudes = amplitudes.clone();
-    sorted_amplitudes.sort_unstable();
+    let mut sorted_points = xy.clone();
+    sorted_points.sort_unstable_by(|&x, &y| compare_by_angle(x, y).reverse());
 
     let solve = |a: usize, b: usize| {
-        let lower = sorted_amplitudes.lower_bound(&amplitudes[a]);
-        let upper = sorted_amplitudes.upper_bound(&amplitudes[b]);
+        let lower = sorted_points
+            .partition_point(|&point| compare_by_angle(point, xy[a]).reverse().is_lt());
+        let upper = sorted_points
+            .partition_point(|&point| compare_by_angle(point, xy[b]).reverse().is_le());
         upper + n * (lower >= upper) as usize - lower
     };
 
@@ -27,51 +23,12 @@ fn main() {
     println!("{output}");
 }
 
-pub mod amplitude {
-    fn gcd(a: i64, b: i64) -> i64 {
-        let mut a = a.abs();
-        let mut b = b.abs();
-
-        while b != 0 {
-            let t = a % b;
-            a = b;
-            b = t;
-        }
-
-        a
-    }
-
-    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-    pub struct Amplitude {
-        x: i64,
-        y: i64,
-    }
-
-    impl PartialOrd for Amplitude {
-        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-            Some(self.cmp(other))
-        }
-    }
-
-    impl Ord for Amplitude {
-        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-            let is_upper_half =
-                |amplitude: Amplitude| amplitude.y > 0 || (amplitude.y == 0 && amplitude.x > 0);
-
-            match (is_upper_half(*self), is_upper_half(*other)) {
-                (true, false) => std::cmp::Ordering::Less,
-                (false, true) => std::cmp::Ordering::Greater,
-                _ => (other.x * self.y).cmp(&(self.x * other.y)),
-            }
-        }
-    }
-
-    impl Amplitude {
-        pub fn new(x: i64, y: i64) -> Self {
-            assert_ne!((x, y), (0, 0));
-
-            let g = gcd(x, y);
-            Self { x: x / g, y: y / g }
-        }
+/// xy平面上の2つの格子点の偏角(x軸正方向を基準とした反時計回りの角度)を比較します。
+pub fn compare_by_angle(point1: (i64, i64), point2: (i64, i64)) -> std::cmp::Ordering {
+    let is_upper_half = |point: (i64, i64)| point.1 > 0 || (point.1 == 0 && point.0 > 0);
+    match (is_upper_half(point1), is_upper_half(point2)) {
+        (true, false) => std::cmp::Ordering::Less,
+        (false, true) => std::cmp::Ordering::Greater,
+        _ => (point2.0 * point1.1).cmp(&(point1.0 * point2.1)),
     }
 }
