295b925a6e8ee904afe3ce660710606be2378d73
pencoder <atcoder8@gmail.com>
2026-09-02 18:58:37 +0900

    Solved ABC473.

--- judge-202510/abc473/src/bin/e.rs
+++ judge-202510/abc473/src/bin/e.rs
@@ -1,7 +1,31 @@
+use std::collections::BTreeSet;
+
+use itertools::enumerate;
 use proconio::input;
 
 fn main() {
     input! {
-        
+        (n, k): (usize, u32),
+        aa: [u32; n],
     }
+
+    let mut acc = vec![0; n + 1];
+    for (i, &a) in enumerate(&aa) {
+        acc[i + 1] = (acc[i] + a) % k;
+    }
+
+    let mut score = 0;
+    let mut previous = BTreeSet::<u32>::from_iter([0]);
+    for right in 1..=n {
+        let prefix_sum = acc[right];
+        if previous.contains(&prefix_sum) {
+            score += 1;
+            previous.clear();
+            previous.insert(prefix_sum);
+        } else {
+            previous.insert(prefix_sum);
+        }
+    }
+
+    println!("{score}");
 }
